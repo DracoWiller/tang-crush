61fd8408ac6001da4396a7e236becb0cd20e4ab0
DracoWiller <88423159+DracoWiller@users.noreply.github.com>
2021-08-04 21:46:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Công chúa của a 🥰🥰🥰🥰🥰 à!',
+    introTitle: 'Cô bé 🥰🥰🥰🥰🥰 à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Làm crs a nhé hic?😘😘😘😘😘😘.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
